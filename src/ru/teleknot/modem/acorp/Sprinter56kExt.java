0e2edee114f97191ccde1f40ccb8607357c6d703
LB426 <odissey@rambler.ru>
2015-12-31 10:57:32 +0300

    first commit

---
+++ src/ru/teleknot/modem/acorp/Sprinter56kExt.java
@@ -0,0 +1,10 @@
+package ru.teleknot.modem.acorp;
+
+/**
+ * Created by lb426 on 31.12.2015.
+ */
+public class Sprinter56kExt {
+    public static void main(String[] args) {
+        System.out.println("test ACORP Sprinter@56k Ext");
+    }
+}
