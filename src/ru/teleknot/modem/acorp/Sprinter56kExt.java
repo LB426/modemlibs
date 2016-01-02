6e1407a61569cd7634bf784174f787fb15ee8cfa
LB426 <odissey@rambler.ru>
2016-01-02 11:37:38 +0300

    добавил исключения при инициализации порта, в конструкторе.

--- src/ru/teleknot/modem/acorp/Sprinter56kExt.java
+++ src/ru/teleknot/modem/acorp/Sprinter56kExt.java
@@ -1,6 +1,8 @@
 package ru.teleknot.modem.acorp;
 
 import com.fazecast.jSerialComm.*;
+
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -10,28 +12,15 @@ import java.util.regex.Pattern;
  */
 public class Sprinter56kExt {
 
-    public Sprinter56kExt(String portName){
+    public Sprinter56kExt(String aPortName) throws IOException
+    {
+        portName = aPortName;
         comPort = SerialPort.getCommPort(portName);
         comPort.setComPortParameters(115200,8,SerialPort.ONE_STOP_BIT,SerialPort.NO_PARITY);
         comPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_BLOCKING, 1000, 0);
         if(comPort.openPort()) {
-            System.out.println("SUCCESS: open COM port " + comPort.getDescriptivePortName());
-        } else {
-            System.out.println("ERROR: open COM port " + comPort.getDescriptivePortName());
-        }
-    }
+            System.out.println("SUCCESS: open COM port " + portName);
 
-    public String getStrFromBuf(byte[] buffer, int length){
-        byte[] buf = new byte[length];
-        for (int i=0; i<length; i++){
-            buf[i] = buffer[i];
-        }
-        String bytesAsString = new String(buf, StandardCharsets.US_ASCII);
-        return bytesAsString;
-    }
-
-    public void Init(){
-        try {
             int comportinitstep = 1;
             //Pattern p = Pattern.compile(".*OK.*", Pattern.DOTALL);
             Pattern p = Pattern.compile("\r\nOK\r\n");
@@ -44,8 +33,8 @@ public class Sprinter56kExt {
                     comPort.writeBytes(ATVCID,ATVCID.length);
                 byte[] readBuffer = new byte[1024];
                 int numRead = comPort.readBytes(readBuffer, readBuffer.length);
-                if(numRead != 0) {
-                    String str = getStrFromBuf(readBuffer,numRead);
+                if (numRead != 0) {
+                    String str = getStrFromBuf(readBuffer, numRead);
                     //System.out.println("str='"+str+"'");
                     Matcher m = p.matcher(str);
                     if (m.matches()) {
@@ -54,40 +43,54 @@ public class Sprinter56kExt {
                 }
             }
             System.out.println("SUCCESS: init");
-        } catch (Exception e) {
-            e.printStackTrace();
+        } else {
+            System.out.println("ERROR: open COM port " + portName);
+            throw new IOException("class: Sprinter56kExt, method: constructor, port no open: " + portName);
         }
     }
 
-    public void Close(){
+    public String getStrFromBuf(byte[] buffer, int length)
+    {
+        if(length > 0) {
+            byte[] buf = new byte[length];
+            for (int i = 0; i < length; i++) {
+                buf[i] = buffer[i];
+            }
+            String bytesAsString = new String(buf, StandardCharsets.US_ASCII);
+            return bytesAsString;
+        } else {
+            return "";
+        }
+    }
+
+    public void Close() throws IOException
+    {
         if(comPort.isOpen()) {
             comPort.closePort();
         } else {
             System.out.println("ERROR: port alredy close " + comPort.getDescriptivePortName());
+            throw new IOException("class: Sprinter56kExt, method: Close, port no open: " + portName);
         }
     }
 
-    public void StartWaitNMBR(){
-        try {
-            Pattern p = Pattern.compile(
-                    "^(\r\nDATE\\s=\\s)([0-9]{0,})(\r\nTIME\\s=\\s)([0-9]{0,})(\r\nNMBR\\s=\\s)([0-9]{0,})(\r\n)$"
-            );
-            while (true){
-                byte[] readBuffer = new byte[1024];
-                int numRead = comPort.readBytes(readBuffer, readBuffer.length);
-                if(numRead != 0) {
-                    String str = getStrFromBuf(readBuffer,numRead);
-                    //System.out.println("str='"+str+"'");
-                    Matcher m = p.matcher(str);
-                    if (m.matches()) {
-                        System.out.println("INCOMING CALL FROM PHONE: " + m.group(6));
-                    }
-                } else {
-                    //System.out.println("READ ZERO byte");
+    public void StartWaitNMBR()
+    {
+        Pattern p = Pattern.compile(
+                "^(\r\nDATE\\s=\\s)([0-9]{0,})(\r\nTIME\\s=\\s)([0-9]{0,})(\r\nNMBR\\s=\\s)([0-9]{0,})(\r\n)$"
+        );
+        while (true){
+            byte[] readBuffer = new byte[1024];
+            int numRead = comPort.readBytes(readBuffer, readBuffer.length);
+            if(numRead != 0) {
+                String str = getStrFromBuf(readBuffer,numRead);
+                //System.out.println("str='"+str+"'");
+                Matcher m = p.matcher(str);
+                if (m.matches()) {
+                    System.out.println("INCOMING CALL FROM PHONE: " + m.group(6));
                 }
+            } else {
+                //System.out.println("READ ZERO byte");
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 
@@ -96,6 +99,7 @@ public class Sprinter56kExt {
     }
 
     private SerialPort comPort;
+    private String portName;
     public static final byte[] AT     = "AT\r\n".getBytes(StandardCharsets.US_ASCII);
     public static final byte[] ATE0   = "ATE=0\r\n".getBytes(StandardCharsets.US_ASCII);
     public static final byte[] ATVCID = "AT+VCID=1\r\n".getBytes(StandardCharsets.US_ASCII);
--- src/ru/teleknot/testmodem/TestModem.java
+++ src/ru/teleknot/testmodem/TestModem.java
@@ -2,15 +2,23 @@ package ru.teleknot.testmodem;
 
 import ru.teleknot.modem.acorp.Sprinter56kExt;
 
+import java.io.IOException;
+
 /**
  * Created by lb426 on 31.12.2015.
  */
 public class TestModem {
     public static void main(String[] args) {
         System.out.println("Testing modem");
-        Sprinter56kExt m = new Sprinter56kExt("COM4");
-        m.Init();
-        m.StartWaitNMBR();
-        m.Close();
+        try {
+            Sprinter56kExt m = new Sprinter56kExt("COM4");
+            m.StartWaitNMBR();
+            m.Close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("finally");
+        }
+        System.out.println("END of Programm Testing modem");
     }
 }
